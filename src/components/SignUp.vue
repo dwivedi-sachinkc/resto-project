e803ff9b3920c8542686fcc39b044488899ecb2b
= <dwivedi.sachinkc@gmail.com>
2021-06-26 14:10:13 +0530

    sign up form done

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,17 +2,43 @@
 <img src="../assets/resto-logo.jpg" class="logo">
   <h1>sign Up</h1>
   <div class="register">
-  <input type="text" name="" id="" placeholder="enter your Name">
-  <input type="text" name="" id="" placeholder="enter your Email">
-  <input type="password" name="" id="" placeholder="enter your Password">
-  <button class="register">Sign up</button>
+  <input type="text" v-model="name" placeholder="enter your Name">
+  <input type="text" v-model="email" placeholder="enter your Email">
+  <input type="password" v-model="password" placeholder="enter your Password">
+  <button class="register" @click="signUp">Sign up</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'Sign Up',
   props: {
+  },
+  data() {
+    return {
+      name:'',
+      emai:'',
+      password:'',
+    }
+  },
+  methods: {
+    async signUp(){
+      console.log("sign", this.name, this.email, this.password);
+      let result = await axios.post("http://localhost:3000/users", {
+        email: this.email,
+        name: this.name,
+        password: this.password,
+      })
+      console.warn(result);
+      if(result.status==201){
+        alert("success")
+      }
+      localStorage.setItem('userInfo', JSON.stringify(result.data));
+      this.name= '';
+      this.email= '';
+      this.password= '';
+    }
   }
 }
 </script>
